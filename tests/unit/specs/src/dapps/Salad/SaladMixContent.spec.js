b3f652b41dbbc1d57db6b69ec2769d8909c41cde
Taariq Levack <taariql@gmail.com>
2020-01-26 07:14:29 +0200

    tests

--- tests/unit/specs/src/dapps/Salad/SaladMixContent.spec.js
+++ tests/unit/specs/src/dapps/Salad/SaladMixContent.spec.js
@@ -23,13 +23,51 @@ describe('SaladMixContent.vue', () => {
     });
   });
 
-
-// todo refactoring
   it('should return the correct data', () => {
     expect(wrapper.vm.$data.page).toEqual('newDeposit');
     expect(wrapper.vm.$data.mixAmount).toEqual('0.01');
     expect(wrapper.vm.$data.deliveryAddress).toEqual('');
     expect(wrapper.vm.$data.isSubmitting).toEqual(false);
+    expect(wrapper.vm.$data.isPending).toEqual(false);
+    expect(wrapper.vm.$data.blockCountdown).toEqual(0);
+    expect(wrapper.vm.$data.quorum).toEqual(0);
+    expect(wrapper.vm.$data.err).toEqual(null);
+    expect(wrapper.vm.$data.deal).toEqual(null);
+    expect(wrapper.vm.$data.dealId).toEqual('');
+    expect(wrapper.vm.$data.dealConfirmed).toEqual(false);
   });
 
+  it('should fail to start deposit with invalid address', async () => {
+    try {
+      await wrapper.vm.startDeposit('foo');
+    } catch (e) {
+      //todo expect error
+    }
+    finally {
+      expect(wrapper.vm.$data.page).toEqual('newDeposit');
+    }
+  });
+
+  it('should start deposit with valid address', async () => {
+    await wrapper.vm.startDeposit(wrapper.vm.account.address);
+    expect(wrapper.vm.$data.page).toEqual('confirmDeposit');
+  });
+
+  it('should cancel deposit', async () => {
+    await wrapper.vm.startDeposit(wrapper.vm.account.address);
+    expect(wrapper.vm.$data.page).toEqual('confirmDeposit');
+
+    await wrapper.vm.cancelDeposit();
+    expect(wrapper.vm.$data.page).toEqual('newDeposit');
+    expect(wrapper.vm.$data.deliveryAddress).toEqual('');
+    expect(wrapper.vm.$data.isSubmitting).toEqual(false);
+  });
+
+  xit('start confirm deposit', async () => {
+    await wrapper.vm.startDeposit(wrapper.vm.account.address);
+    expect(wrapper.vm.$data.page).toEqual('confirmDeposit');
+
+    // todo mock salad and test 
+    await wrapper.vm.confirmDeposit();
+  });
 });
